fb7b5d66924961b5a429b44c7ba86fad46c437dc
peppone-choi <peppone.choi@gmail.com>
2024-08-20 13:49:52 +0900

    [최병호] 타입 지정

--- src/home.ts
+++ src/home.ts
@@ -11,7 +11,7 @@ const OPEN_WEATHER_APP_ID = "63d4d5b5c3cc0f33e94516f7ef477238";
 const TMDB_BEARER_TOKEN =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2FiODczNDg0MGM3ZTg0YmRlZjg3ZThjY2EwYWNjYyIsIm5iZiI6MTcyMzUwNjM1MC41OTA3NjUsInN1YiI6IjY2MTBlMTMzYjA5YmRlMDE3ZWJjYmE3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.91Lkyh_Yp1fow-wBetsYkgGhp6Nvw5sm8GnutDrAAE0";
 (async () => {
-  const location = await getLocation();
+  const location: Location = await getLocation();
   const WeatherData: Weather = await getWeather(location, OPEN_WEATHER_APP_ID);
   const movie: getNowPlayingAndUpcomingMoviesResponse = await getNowPlayingAndUpcomingMovies(1, TMDB_BEARER_TOKEN);
 })();

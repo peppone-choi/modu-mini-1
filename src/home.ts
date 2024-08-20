93def09da37d7125e6f99a9282ee1e358baf0368
peppone-choi <peppone.choi@gmail.com>
2024-08-20 17:07:08 +0900

    [최병호] API키 분리

--- src/home.ts
+++ src/home.ts
@@ -6,11 +6,7 @@ import { getLocation } from "./utils/location.util";
 import { getWeather } from "./utils/weather.util";
 import { getNowPlayingAndUpcomingMovies } from "./utils/movie.util";
 import { getNowPlayingAndUpcomingMoviesResponse } from "./@types/movie.type";
-
-const OPEN_WEATHER_APP_ID = "63d4d5b5c3cc0f33e94516f7ef477238";
-const TMDB_BEARER_TOKEN =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2FiODczNDg0MGM3ZTg0YmRlZjg3ZThjY2EwYWNjYyIsIm5iZiI6MTcyMzUwNjM1MC41OTA3NjUsInN1YiI6IjY2MTBlMTMzYjA5YmRlMDE3ZWJjYmE3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.91Lkyh_Yp1fow-wBetsYkgGhp6Nvw5sm8GnutDrAAE0";
-const KAKAO_APP_KEY = "d1b5860c56f56b199f2934b432965880";
+import { KAKAO_APP_KEY, OPEN_WEATHER_APP_ID, TMDB_BEARER_TOKEN } from "./config/config";
 
 (async () => {
   const location: Location = await getLocation();

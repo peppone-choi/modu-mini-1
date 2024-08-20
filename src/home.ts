e35eb11885e7f59330c9c5dbae3702c7d34d73ea
peppone-choi <peppone.choi@gmail.com>
2024-08-20 13:49:24 +0900

    [최병호] 타입 지정

--- src/home.ts
+++ src/home.ts
@@ -5,15 +5,13 @@ import { Weather } from "./@types/weather.type";
 import { getLocation } from "./utils/location.util";
 import { getWeather } from "./utils/weather.util";
 import { getNowPlayingAndUpcomingMovies } from "./utils/movie.util";
+import { getNowPlayingAndUpcomingMoviesResponse } from "./@types/movie.type";
 
 const OPEN_WEATHER_APP_ID = "63d4d5b5c3cc0f33e94516f7ef477238";
 const TMDB_BEARER_TOKEN =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2FiODczNDg0MGM3ZTg0YmRlZjg3ZThjY2EwYWNjYyIsIm5iZiI6MTcyMzUwNjM1MC41OTA3NjUsInN1YiI6IjY2MTBlMTMzYjA5YmRlMDE3ZWJjYmE3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.91Lkyh_Yp1fow-wBetsYkgGhp6Nvw5sm8GnutDrAAE0";
 (async () => {
   const location = await getLocation();
-  console.log(location);
   const WeatherData: Weather = await getWeather(location, OPEN_WEATHER_APP_ID);
-  console.log(WeatherData);
-  const movie = await getNowPlayingAndUpcomingMovies(1, TMDB_BEARER_TOKEN);
-  console.log(movie);
+  const movie: getNowPlayingAndUpcomingMoviesResponse = await getNowPlayingAndUpcomingMovies(1, TMDB_BEARER_TOKEN);
 })();

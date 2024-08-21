19de6457af1a6e6006c34e91138cd122cf5c0aa8
peppone-choi <peppone.choi@gmail.com>
2024-08-21 09:36:53 +0900

    [최병호] todo 모델 주석 추가

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -1,27 +1,27 @@
 export class Todo {
   static id = 0;
-  id: number;
-  content: string;
-  day: string;
-  time: string;
-  isCompleted: boolean;
+  id: number; // 할 일 고유번호
+  content: string; // 할 일 내용
+  day: string; // 할 일 날짜
+  time: string; // 할 일 시간
+  isCompleted: boolean; // 완료 여부
   constructor(content: string, day: string, time: string) {
     this.id = ++Todo.id;
     this.content = content;
     this.day = day;
     this.time = time;
     this.isCompleted = false;
-  }
+  } // 생성자
   toggleCompleted() {
     this.isCompleted = !this.isCompleted;
-  }
+  } // 완료여부 토글
   updateContent(content: string) {
     this.content = content;
-  }
+  } // 내용 수정
   updateDay(day: string) {
     this.day = day;
-  }
+  } // 날짜 수정
   updateTime(time: string) {
     this.time = time;
-  }
+  } // 시간 수정
 }

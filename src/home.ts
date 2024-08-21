68d705b502a636c5891126fe8916ccbc0f0687b5
peppone-choi <peppone.choi@gmail.com>
2024-08-21 12:13:48 +0900

    [최병호] 날씨 아이콘 출력

--- src/home.ts
+++ src/home.ts
@@ -6,8 +6,8 @@ import { getLocation } from "./utils/location.util";
 import { getWeather } from "./utils/weather.util";
 import { getNowPlayingAndUpcomingMovies } from "./utils/movie.util";
 import { getNowPlayingAndUpcomingMoviesResponse } from "./@types/movie.type";
-import { KAKAO_APP_KEY, OPEN_WEATHER_APP_ID, TMDB_BEARER_TOKEN } from "./config/config";
-import { $ } from "./elements/home.element.ts";
+import { KAKAO_APP_KEY, OPEN_WEATHER_APP_ID, TMDB_BEARER_TOKEN, weatherIconMap } from "./config/config";
+import "./elements/home.element.ts";
 
 (async () => {
   const location: Location = await getLocation();
@@ -20,18 +20,17 @@ import { $ } from "./elements/home.element.ts";
   temp.textContent = "온도 : " + weatherData.temp;
   weather.textContent = "날씨 : " + weatherData.weather;
   pollution.textContent = "미세먼지 : " + weatherData.pollution;
-
-  
-  movieData.results.slice(0, 3 ).forEach((movieItem) => {
+  weatherIcon.src = `../public/img/weather/Light bg/${weatherIconMap.get(weatherData.weatherIcon)}`;
+  movieData.results.slice(0, 3).forEach((movieItem) => {
     const liElement = document.createElement("li");
 
     liElement.classList.add("movieItem");
 
     const imgElement = document.createElement("img");
 
-    imgElement.src = 'https://image.tmdb.org/t/p/w300' + movieItem.poster_path;
-    
-    console.log('https://image.tmdb.org/t/p/w500' + movieItem.poster_path);
+    imgElement.src = "https://image.tmdb.org/t/p/w300" + movieItem.poster_path;
+
+    console.log("https://image.tmdb.org/t/p/w500" + movieItem.poster_path);
     imgElement.alt = movieItem.title;
 
     const titleText = document.createTextNode(movieItem.title);
@@ -40,9 +39,5 @@ import { $ } from "./elements/home.element.ts";
     liElement.appendChild(titleText);
 
     movies.appendChild(liElement);
-
   });
-
-
-
 })();

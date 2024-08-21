ffe45a9902ba7ac4344ba061a7ff5493915aa283
peppone-choi <peppone.choi@gmail.com>
2024-08-21 16:23:14 +0900

    [최병호] 로컬 스토리지 newTodo and oldTodo 저장

--- src/todo.ts
+++ src/todo.ts
@@ -2,7 +2,7 @@ import dayjs from "dayjs";
 import "./main";
 import "./styles/todo.scss";
 import { Todo } from "./models/todo.model";
-import { Value } from "sass";
+
 const timeline = document.querySelector("#timeline");
 const dayTimeList: Array<string> = [];
 const timelineHtmlList: Array<string> = [];
@@ -13,6 +13,7 @@ const taskStartDate = document.querySelector("#start-date");
 const taskStartTime = document.querySelector("#start-time");
 const taskEndTime = document.querySelector("#end-time");
 const addTaskItemButton = document.querySelector(".add-task-item");
+const oldTodo = JSON.parse(localStorage.getItem("todo") as string); // 로컬스토리지에 있는 todo를 가져와서 파싱
 
 for (let i = 0; i < 24; i++) {
   dayTimeList.push(`${String(i).padStart(2, "0")}:00`);
@@ -64,13 +65,12 @@ addTaskItemButton?.addEventListener("click", () => {
     startTime: taskStartTime.value,
     endTime: taskEndTime.value,
   });
-  console.log(newTodo);
   if (!localStorage.getItem("todo")) {
     // 로컬스토리지에 todo가 없을 때
     localStorage.setItem("todo", JSON.stringify([newTodo])); // 새로운 todo를 배열로 만들어서 저장
   } else {
+    console.log(oldTodo);
     // 로컬스토리지에 todo가 있을 때
-    const oldTodo = JSON.parse(localStorage.getItem("todo") as string); // 로컬스토리지에 있는 todo를 가져와서 파싱
     const newTodoList = [...oldTodo, newTodo]; // 기존 todo와 새로운 todo를 합침
     localStorage.setItem("todo", JSON.stringify(newTodoList)); // 합친 todo를 다시 저장
   }

cbd7af24e7dfdd7648b246250dddb3c51c85a0e7
GyeonghoJeon <wjstkwal7@gmail.com>
2024-08-21 11:19:58 +0900

    weather, movie 데이터 연결

--- src/home.ts
+++ src/home.ts
@@ -7,14 +7,42 @@ import { getWeather } from "./utils/weather.util";
 import { getNowPlayingAndUpcomingMovies } from "./utils/movie.util";
 import { getNowPlayingAndUpcomingMoviesResponse } from "./@types/movie.type";
 import { KAKAO_APP_KEY, OPEN_WEATHER_APP_ID, TMDB_BEARER_TOKEN } from "./config/config";
-import "./elements/home.element.ts";
+import { $ } from "./elements/home.element.ts";
 
 (async () => {
-  console.log(movies);
   const location: Location = await getLocation();
   console.log(location);
   const weatherData: Weather = await getWeather(location, OPEN_WEATHER_APP_ID, KAKAO_APP_KEY);
   console.log(weatherData);
-  const movie: getNowPlayingAndUpcomingMoviesResponse = await getNowPlayingAndUpcomingMovies(1, TMDB_BEARER_TOKEN);
-  console.log(movie);
+  const movieData: getNowPlayingAndUpcomingMoviesResponse = await getNowPlayingAndUpcomingMovies(1, TMDB_BEARER_TOKEN);
+  console.log(movieData);
+
+  temp.textContent = "온도 : " + weatherData.temp;
+  weather.textContent = "날씨 : " + weatherData.weather;
+  pollution.textContent = "미세먼지 : " + weatherData.pollution;
+
+  
+  movieData.results.slice(0, 3 ).forEach((movieItem) => {
+    const liElement = document.createElement("li");
+
+    liElement.classList.add("movieItem");
+
+    const imgElement = document.createElement("img");
+
+    imgElement.src = 'https://image.tmdb.org/t/p/w300' + movieItem.poster_path;
+    
+    console.log('https://image.tmdb.org/t/p/w500' + movieItem.poster_path);
+    imgElement.alt = movieItem.title;
+
+    const titleText = document.createTextNode(movieItem.title);
+
+    liElement.appendChild(imgElement);
+    liElement.appendChild(titleText);
+
+    movies.appendChild(liElement);
+
+  });
+
+
+
 })();
